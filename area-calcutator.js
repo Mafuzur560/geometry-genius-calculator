1189edff083b8a607879fad007d8cf02cf2bd8b2
Mafuzur560 <mrridoy461@gmail.com>
2023-08-18 16:06:39 +0600

    tringle calculator complited

--- area-calcutator.js
+++ area-calcutator.js
@@ -1,1 +1,19 @@
-console.log('connected');
+function calculateTringleArea(){
+    const baseInputfild = document.getElementById('tringle-base');
+    const baseInputText = baseInputfild.value ;
+    const base = parseFloat(baseInputText);
+
+    // 02
+    const heightInputFild = document.getElementById('tringle-height');
+    const heightInputText = heightInputFild.value;
+    const height = parseFloat(heightInputText);
+
+    const area = 0.5 * base * height ;
+
+    // 03
+    const areaFild = document.getElementById('tringle-area');
+    areaFild.innerText = area;
+
+    base.innerText = '';
+}
+
